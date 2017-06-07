d5ca4c075d145376d3b92ec6504c60c43edb2706
陈海荣 <chenhairong@sunlight.bz>
2017-06-07 16:14:40 +0800

    添加项目文件。

---
+++ RedisInAction/Controllers/ArticleController.cs
@@ -0,0 +1,116 @@
+﻿using RedisInAction.Models;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace RedisInAction.Controllers
+{
+    public class ArticleController : ApiController
+    {
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            var cacheConnection = ConfigurationManager.AppSettings["CacheConnection"];
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection => LazyConnection.Value;
+
+        protected IDatabase Cache { get; } = Connection.GetDatabase();
+
+        // GET: api/Article
+        public IHttpActionResult Get([FromUri]int pageIndex, [FromUri]int pageElements, [FromUri]string order= "score")
+        {
+            var start = (pageIndex - 1) * pageElements;
+            var end = start + pageElements - 1;
+            var ids = Cache.SortedSetRangeByRank($"{order}:", start, end, Order.Descending);
+            var articles = new List<Article>();
+            foreach (var id in ids)
+            {
+                var article = new Article();
+                var articleData = Cache.HashGetAll(id.ToString());
+                foreach (var entry in from ad in articleData
+                                      join a in article.GetType().GetProperties() on ad.Name.ToString() equals a.Name.ToLower()
+                                      select new { a, ad.Value })
+                {
+                    if(entry.a.PropertyType == typeof(int))
+                        entry.a.SetValue(article, int.Parse(entry.Value.ToString()));
+                    else
+                        entry.a.SetValue(article, entry.Value.ToString());
+                }
+                articles.Add(article);
+            }
+            return Ok(articles);
+        }
+
+        // GET: api/Article/5
+        public string Get(int id)
+        {
+            return "value2";
+        }
+
+        // POST: api/Article
+        public IHttpActionResult Post([FromUri]string title, [FromUri]int userId, [FromUri]string link)
+        {
+            var articleId = Cache.StringIncrement("article:");
+            var voted = $"voted:{articleId}";
+            Cache.SetAdd(voted, $"user:{userId}");
+            Cache.KeyExpire(voted, new TimeSpan(7, 0, 0, 0));
+            var publishTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+            var article = $"article:{articleId}";
+            Cache.HashSet(article, new[]
+            {
+                new HashEntry("id", articleId),
+                new HashEntry("title", title),
+                new HashEntry("link", link),
+                new HashEntry("poster", $"user:{userId}"),
+                new HashEntry("time", publishTime),
+                new HashEntry("votes", 1),
+            });
+            Cache.SortedSetAdd("score:", article, publishTime + VOTE_SCORE);
+            Cache.SortedSetAdd("time:", article, publishTime);
+            return Ok(articleId);
+        }
+
+        // PUT: api/Article/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Article/5
+        public void Delete(int id)
+        {
+        }
+
+        private const int VOTE_SCORE = 86400 / 200;
+
+        [HttpPost]
+        public IHttpActionResult Vote(int id, [FromUri]int userId)
+        {
+            var articleKey = $"article:{id}";
+            if (!Cache.KeyExists(articleKey))
+                return BadRequest("article does not exist");
+            var cutoff = DateTimeOffset.Now.AddDays(-7).ToUnixTimeSeconds();
+            var articleTime = Cache.SortedSetScore("time:", articleKey);
+            if (!articleTime.HasValue)
+                return BadRequest("article's publish time lost");
+            if (cutoff > articleTime.Value)
+                return BadRequest("out of time");
+            if (!Cache.SetAdd($"voted:{id}", $"user:{userId}"))
+                return BadRequest("you have already voted this article");
+            Cache.SortedSetIncrement("score:", articleKey, VOTE_SCORE);
+            return Ok(Cache.HashIncrement(articleKey, "votes"));
+        }
+
+        public IHttpActionResult AddRemoveGroup()
+        {
+            
+            return Ok();
+        }
+    }
+}
---
+++ RedisInAction/Models/Article.cs
@@ -0,0 +1,12 @@
+﻿namespace RedisInAction.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Poster { get; set; }
+        public int Time { get; set; }
+        public int Votes { get; set; }
+    }
+}
