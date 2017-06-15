ebe984f4aec27fb4e31a4ed92828718a81cb77cf
陈海荣 <chrsas@outlook.com>
2017-06-15 14:32:36 +0800

    增加修改Token的性能测试

--- RedisInAction/Controllers/TokenController.cs
+++ RedisInAction/Controllers/TokenController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
 using StackExchange.Redis;
 
@@ -38,6 +41,23 @@ namespace RedisInAction.Controllers
             Redis.SortedSetIncrement("viewed:", item, 1);
         }
 
+        [HttpPost]
+        // POST: api/Token/Batch
+        public void Batch([FromUri]string token, [FromUri]int userId, [FromUri]string item)
+        {
+            var timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+            var batch = Redis.CreateBatch();
+            batch.HashSetAsync("login:", token, userId);
+            batch.SortedSetAddAsync("recent:", token, timestamp);
+            if (item == null)
+                return;
+            batch.SortedSetAddAsync($"viewed:{token}", item, timestamp);
+            // 只保留最近浏览的25个
+            batch.SortedSetRemoveRangeByRankAsync($"viewed:{token}", 25, -1);
+            batch.SortedSetIncrementAsync("viewed:", item, 1);
+            batch.Execute();
+        }
+
         // PUT: api/Token/5
         public void Put(int id, [FromBody]string value)
         {
@@ -52,6 +72,31 @@ namespace RedisInAction.Controllers
         public IHttpActionResult Check([FromUri]string token)
         {
             return Ok(Redis.HashGet("login:", token));
+        }
+
+        [HttpPost]
+        public IHttpActionResult BenchmarkUpdate([FromUri]int duration)
+        {
+            var methodes = new List<Tuple<Action<string, int, string>, string>>
+            {
+                new Tuple<Action<string, int, string>, string>(this.Post, nameof(this.Post)),
+                new Tuple<Action<string, int, string>, string>(Batch, nameof(this.Batch))
+            };
+            var strList = new List<string>();
+            foreach (var method in methodes)
+            {
+                var count = 0;
+                var start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                var end = start + duration * 1000;
+                while (DateTimeOffset.Now.ToUnixTimeMilliseconds() < end)
+                {
+                    count++;
+                    method.Item1.Invoke("token", 111, "item");
+                }
+                var delta = DateTimeOffset.Now.ToUnixTimeMilliseconds() - start;
+                strList.Add($"{method.Item2}, {count}, {delta}, {count / (delta / 1000)}");
+            }
+            return Ok(strList);
         }
     }
 }
