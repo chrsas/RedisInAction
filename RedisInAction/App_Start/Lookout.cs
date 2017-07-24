8bced734638d7285d8e2d105acb80be384fbfa5f
陈海荣 <chrsas@outlook.com>
2017-07-24 11:30:55 +0800

    增加异常处理

--- RedisInAction/App_Start/Lookout.cs
+++ RedisInAction/App_Start/Lookout.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Linq;
 using Newtonsoft.Json;
 using RedisInAction.App_Data;
+using RedisInAction.Controllers;
 using StackExchange.Redis;
 
 namespace RedisInAction {
@@ -19,40 +20,48 @@ namespace RedisInAction {
         public static void CleanFullSessions() {
             var redis = RedisConnectionHelp.Connection.GetDatabase();
             while(!quit) {
-                var size = redis.SortedSetLength("recent:");
-                if(size <= LIMIT) {
-                    Thread.Sleep(1000);
-                    continue;
+                try {
+                    var size = redis.SortedSetLength("recent:");
+                    if(size <= LIMIT) {
+                        Thread.Sleep(1000);
+                        continue;
+                    }
+                    var endIndex = Math.Min(size - LIMIT, 100);
+                    var tokens = redis.SortedSetRangeByRank("recent:", 0, endIndex - 1);
+                    var sessionKeys = tokens.Select(t => (RedisKey)$"viewed:{t}").
+                        Union(tokens.Select(t => (RedisKey)$"cart:{t}")).ToArray();
+                    redis.KeyDelete(sessionKeys);
+                    redis.HashDelete("login:", tokens);
+                    redis.SortedSetRemove("recent:", tokens);
+                } catch(RedisTimeoutException e) {
+                    new LogController { Redis = redis }.Post(nameof(RedisTimeoutException), e.Message);
                 }
-                var endIndex = Math.Min(size - LIMIT, 100);
-                var tokens = redis.SortedSetRangeByRank("recent:", 0, endIndex - 1);
-                var sessionKeys = tokens.Select(t => (RedisKey)$"viewed:{t}").
-                    Union(tokens.Select(t => (RedisKey)$"cart:{t}")).ToArray();
-                redis.KeyDelete(sessionKeys);
-                redis.HashDelete("login:", tokens);
-                redis.SortedSetRemove("recent:", tokens);
             }
         }
 
         public static void CacheRows() {
             var redis = RedisConnectionHelp.Connection.GetDatabase();
             while(!quit) {
-                var next = redis.SortedSetRangeByRankWithScores("schedule:", 0, 0).FirstOrDefault();
-                var timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
-                if(!next.Element.HasValue || next.Score > timestamp) {
-                    Thread.Sleep(50);
-                    continue;
+                try {
+                    var next = redis.SortedSetRangeByRankWithScores("schedule:", 0, 0).FirstOrDefault();
+                    var timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+                    if(!next.Element.HasValue || next.Score > timestamp) {
+                        Thread.Sleep(50);
+                        continue;
+                    }
+                    var delay = redis.SortedSetScore("delay:", next.Element);
+                    if(delay == null || delay < 0) {
+                        redis.SortedSetRemove("delay:", next.Element);
+                        redis.SortedSetRemove("schedule:", next.Element);
+                        redis.KeyDelete($"inv:{next.Element}");
+                        continue;
+                    }
+                    var row = Inventory.Get(next.Element);
+                    redis.SortedSetAdd("schedule:", next.Element, timestamp + delay.Value);
+                    redis.StringSet($"inv:{next.Element}", JsonConvert.SerializeObject(row));
+                } catch(RedisTimeoutException e) {
+                    new LogController { Redis = redis }.Post(nameof(RedisTimeoutException), e.Message);
                 }
-                var delay = redis.SortedSetScore("delay:", next.Element);
-                if(delay == null || delay < 0) {
-                    redis.SortedSetRemove("delay:", next.Element);
-                    redis.SortedSetRemove("schedule:", next.Element);
-                    redis.KeyDelete($"inv:{next.Element}");
-                    continue;
-                }
-                var row = Inventory.Get(next.Element);
-                redis.SortedSetAdd("schedule:", next.Element, timestamp + delay.Value);
-                redis.StringSet($"inv:{next.Element}", JsonConvert.SerializeObject(row));
             }
         }
 
@@ -60,9 +69,13 @@ namespace RedisInAction {
             var redis = RedisConnectionHelp.Connection.GetDatabase();
             while(!quit) {
                 // 删除所有排名在20000名之后的商品
-                redis.SortedSetRemoveRangeByRank("viewed:", 20000, -1);
-                redis.SortedSetCombineAndStore(SetOperation.Intersect, "viewed:", new RedisKey[] { "viewed:" }, new[] { 0.5 });
-                Thread.Sleep(new TimeSpan(0, 5, 0));
+                try {
+                    redis.SortedSetRemoveRangeByRank("viewed:", 20000, -1);
+                    redis.SortedSetCombineAndStore(SetOperation.Intersect, "viewed:", new RedisKey[] { "viewed:" }, new[] { 0.5 });
+                    Thread.Sleep(new TimeSpan(0, 5, 0));
+                } catch(RedisTimeoutException e) {
+                    new LogController { Redis = redis }.Post(nameof(RedisTimeoutException), e.Message);
+                }
             }
         }
     }
---
+++ RedisInAction/Controllers/LogController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StackExchange.Redis;
+
+namespace RedisInAction.Controllers {
+
+    public class LogController : ApiController {
+        internal IDatabase Redis { get; set; } = RedisConnectionHelp.Connection.GetDatabase();
+        public const string DEBUG = "debug";
+        public const string INFO = "info";
+        public const string WARNING = "warning";
+        public const string ERROR = "error";
+        public const string CRITICAL = "critical";
+
+        // GET: api/Log
+        public IEnumerable<string> Get() {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Log/5
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST: api/Log
+        public void Post([FromUri]string name, [FromUri]string message, [FromUri]string severity = INFO, IBatch pipeline = null) {
+            var destination = $"recent:{name}:{severity}";
+            message = $"{DateTime.Now} {message}";
+            var batch = pipeline ?? Redis.CreateBatch();
+            batch.ListLeftPushAsync(destination, message);
+            batch.ListTrimAsync(destination, 0, 99);
+            batch.Execute();
+        }
+
+        // PUT: api/Log/5
+        public void Put(int id, [FromBody]string value) {
+        }
+
+        // DELETE: api/Log/5
+        public void Delete(int id) {
+        }
+    }
+}
